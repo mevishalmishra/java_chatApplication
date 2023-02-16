879fa4ede0e88bec43cc9de78091706d5314d309
vishal <generousvishal@gmail.com>
2023-02-16 22:53:49 +0530

    gui ready for both client and server

--- Client.java
+++ Client.java
@@ -1,11 +1,26 @@
 import java.io.*;
 import java.net.*;
 
-public class Client {
-    Socket socket;
+import java.awt.*;
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.*;
+
+public class Client extends JFrame {
+
+    // chat
+    Socket socket;
     BufferedReader bReader;
     PrintWriter pWriter;
+
+    // component declaration for gui-swing
+    private JFrame chatClientFrame = new JFrame("Client");
+    private JLabel chatClientHeading = new JLabel("Client");
+    private JTextArea chatClientTextArea = new JTextArea();
+    private JTextField chatClientMessage = new JTextField();
+    private Font chatClientFont = new Font("Roboto", Font.PLAIN, 20);
 
     public Client() {
 
@@ -16,12 +31,86 @@ public class Client {
 
             bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pWriter = new PrintWriter(socket.getOutputStream());
-
+            createCLientSideChatGUI();
+            handleChatEvents();
             startReading();
             startWriting();
         } catch (Exception e) {
             // TODO: handle exception
+            e.printStackTrace();
         }
+
+    }
+
+    // handle chat events
+    private void handleChatEvents() {
+        chatClientMessage.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // System.out.println("ok");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // System.out.println("ok");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+                if (e.getKeyCode() == 10) {
+                    String messageToSend = chatClientMessage.getText();
+                    System.out.println(messageToSend);
+                    chatClientTextArea.append("You: " + messageToSend + "\n");
+                    pWriter.println(messageToSend);
+                    pWriter.flush();
+                    chatClientMessage.setText("");
+                    chatClientMessage.requestFocus();
+                }
+            }
+        });
+
+    }
+
+    // method to call client chat gui
+    private void createCLientSideChatGUI() {
+
+        // this.chatClientHeading = new JLabel("Client Chat [END]");
+        this.setTitle("Client Chat [END]");
+        this.setSize(600, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        // set font
+        chatClientFrame.setFont(chatClientFont);
+        chatClientHeading.setFont(chatClientFont);
+        chatClientMessage.setFont(chatClientFont);
+        chatClientTextArea.setFont(chatClientFont);
+
+        // set img icon
+        ImageIcon imageIcon = new ImageIcon("logo.png");
+        Image scaledImage = imageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        chatClientHeading.setIcon(new ImageIcon(scaledImage));
+
+        // // set headingtext to below of icon
+        chatClientHeading.setHorizontalTextPosition(SwingConstants.CENTER);
+        chatClientHeading.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+        // layout
+        this.setLayout(new BorderLayout());
+
+        // adding components to frame
+        this.add(chatClientHeading, BorderLayout.NORTH);
+        this.add(chatClientTextArea, BorderLayout.CENTER);
+        this.add(chatClientMessage, BorderLayout.SOUTH);
+
+        // alignement
+        chatClientHeading.setHorizontalAlignment(SwingConstants.CENTER);
+        chatClientHeading.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
     }
 
@@ -40,12 +129,17 @@ public class Client {
 
                     if (msg.equals("exit")) {
                         System.out.println("Server terminated the chat.");
+
+                        JOptionPane.showMessageDialog(this, "Server terminated the chat.");
+                        chatClientMessage.setEnabled(false);
                         break;
                     }
 
                     // write msg on console, as it is sent by client
 
-                    System.out.println("Server : " + msg);
+                    // System.out.println("Server : " + msg);
+
+                    chatClientTextArea.append("Server says: " + msg + "\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- Server.java
+++ Server.java
@@ -1,12 +1,14 @@
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.*;
 
-class Server {
+import java.awt.*;
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+class Server extends JFrame {
 
     ServerSocket server;
     Socket socket;
@@ -15,6 +17,13 @@ class Server {
 
     BufferedReader bReader;
     PrintWriter pWriter;
+
+    // component declaration for gui-swing
+    private JFrame chatServerFrame = new JFrame("Client");
+    private JLabel chatSeverHeading = new JLabel("Client");
+    private JTextArea chatServerTextArea = new JTextArea();
+    private JTextField chatServerMessage = new JTextField();
+    private Font chatServerFont = new Font("Roboto", Font.PLAIN, 20);
 
     public Server() {
         try {
@@ -37,12 +46,85 @@ class Server {
 
             bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pWriter = new PrintWriter(socket.getOutputStream());
-
+            createServerSideChatGUI();
+            handleChatEvents();
             startReading();
             startWriting();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // handle chat events
+    private void handleChatEvents() {
+        chatServerMessage.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // System.out.println("ok");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // System.out.println("ok");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+                if (e.getKeyCode() == 10) {
+                    String messageToSend = chatServerMessage.getText();
+                    System.out.println(messageToSend);
+                    chatServerTextArea.append("You: " + messageToSend + "\n");
+                    pWriter.println(messageToSend);
+                    pWriter.flush();
+                    chatServerMessage.setText("");
+                    chatServerMessage.requestFocus();
+                }
+            }
+        });
+
+    }
+
+    // method to call client chat gui
+    private void createServerSideChatGUI() {
+
+        // this.chatClientHeading = new JLabel("Client Chat [END]");
+        this.setTitle("Server Chat [END]");
+        this.setSize(600, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        // set font
+        chatServerFrame.setFont(chatServerFont);
+        chatSeverHeading.setFont(chatServerFont);
+        chatServerMessage.setFont(chatServerFont);
+        chatServerTextArea.setFont(chatServerFont);
+
+        // set img icon
+        ImageIcon imageIcon = new ImageIcon("logo.png");
+        Image scaledImage = imageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        chatSeverHeading.setIcon(new ImageIcon(scaledImage));
+
+        // // set headingtext to below of icon
+        chatSeverHeading.setHorizontalTextPosition(SwingConstants.CENTER);
+        chatSeverHeading.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+        // layout
+        this.setLayout(new BorderLayout());
+
+        // adding components to frame
+        this.add(chatSeverHeading, BorderLayout.NORTH);
+        this.add(chatServerTextArea, BorderLayout.CENTER);
+        this.add(chatServerMessage, BorderLayout.SOUTH);
+
+        // alignement
+        chatSeverHeading.setHorizontalAlignment(SwingConstants.CENTER);
+        chatSeverHeading.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
     }
 
     // concept of multithreading is used to run both the things simultaneously
@@ -60,12 +142,13 @@ class Server {
 
                     if (msg.equals("exit")) {
                         System.out.println("Client terminated the chat.");
+                        JOptionPane.showMessageDialog(this, "Client terminated the chat.");
                         break;
                     }
 
                     // write msg on console, as it is sent by client
 
-                    System.out.println("Client : " + msg);
+                    chatServerTextArea.append("Client says: " + msg + "\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
